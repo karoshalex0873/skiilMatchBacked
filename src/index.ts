421abbc678347654758044103b4f773247977222
Alex Karobia <akarobia30@gmail.com>
2025-04-14 17:30:15 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -25,12 +25,22 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 
+// app.use(cors({
+//   origin: ["http://localhost:4200", "http://dkskillmatch.s3-website.eu-north-1.amazonaws.com", "https://skill-matc-ai-frontend.vercel.app"],
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Include OPTIONS
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true
+// }))
 app.use(cors({
-  origin: ["http://localhost:4200", "http://dkskillmatch.s3-website.eu-north-1.amazonaws.com", "https://skill-matc-ai-frontend.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Include OPTIONS
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}))
+  origin: [
+    "http://localhost:4200",
+    "http://dkskillmatch.s3-website.eu-north-1.amazonaws.com",
+    "https://skill-matc-ai-frontend.vercel.app"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 // welcome message
